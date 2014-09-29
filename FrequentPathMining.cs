d92b4c4de61ed57d5742e2fa92e7d695a5ae6270
uqjhan5 <uqjhan5@ba0cf054-b380-9f48-870c-e6315e86d8ef>
2014-09-29 06:33:20 +0000

    fixed StepType GetHashCode collision bug
    git-svn-id: file:///d/svn/HORM/FNM_Undirected@42 ba0cf054-b380-9f48-870c-e6315e86d8ef

--- FrequentPathMining.cs
+++ FrequentPathMining.cs
@@ -16,7 +16,7 @@ namespace FNM_Undirected
         public Step(int id, StepType type) : base(id, type) { }
         public override int GetHashCode()
         {
-            return Item1.GetHashCode() & Item2.GetHashCode();
+            return Item1.GetHashCode() | Item2.GetHashCode();
         }
         public override bool Equals(object obj)
         {
